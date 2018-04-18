978bfbdec4a847cba1a5a199d18e1006e683a62c
Denis Morielli <dm44606@rona.ca>
2018-04-17 23:42:48 -0400

    Added Keyvault Service in ARM Template and Azure AD App for Keyvault in PS installation script.

--- ARM/deploy-arm.ps1
+++ ARM/deploy-arm.ps1
@@ -7,9 +7,12 @@ Write-Host " Azure OnboardCheck Assets " -ForegroundColor White
 Write-Host " Azure ARM Resources Setup " -ForegroundColor White
 Write-Host "***************************" -ForegroundColor Blue
 
+# Import external functions
+. .\EnsureAzureAdApp.ps1
 # Import input configuration from json configuration file
 $devConfig = (Get-Content "deployment-params.json" -Raw) | ConvertFrom-Json
-
+# Set config variables
+$tenantName = $devConfig.tenantname
 $acctName = $devConfig.azureaccount
 $subscriptionId = $devConfig.subscriptionid
 $resourceGroupName = $devConfig.resourcegroupname
@@ -29,23 +32,36 @@ if ($devConfig.azureaccount) {
     Write-Host "Using $($psCred.UserName) to login to Azure..."
     $profile = Login-AzureRmAccount -Credential $psCred  -SubscriptionId $subscriptionId
 } else {
+    $psCred = Get-Credential
     $profile = Login-AzureRmAccount
 }
 
 Get-AzureSubscription
 Select-AzureRmSubscription -SubscriptionId $subscriptionId
 $confirmation = Read-Host "Confirm using subscription above to create $($devConfig.resourcegroupname)? (y/n)"
+
 if ($confirmation.ToLower() -eq "y") {
+    #### DEPLOY THE ARM TEMPLATE ####
     # Create the Resource Group which will contain all resources
     New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
-    # Use the resource group, template file, and template parameters to create the resources
+    # Use the resource group, template file, and template parameters to create the resources and create an output
     New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateFile "arm-onboardcheck.json" -TemplateParameterFile "arm-parameters.json" | ConvertTo-Json | Out-File "arm-outputs.json"
     $armOutput = (Get-Content "arm-outputs.json" -Raw) | ConvertFrom-Json
-    $armOutput.outputs.ConfigurationStorageAccountKey.value
-    @{subscriptionid = $subscriptionId;
-    storageconnection = $armOutput.outputs.ConfigurationStorageAccountKey.value;
-    resourcegroupname = $resourceGroupName;
-    functionappname = $armOutput.outputs.FunctionAppName.value;} | ConvertTo-Json | Out-File "deploy.config.json"
+    $armOutput.outputs.functionAppName.value
+
+    #### DEPLOY THE AZURE AD APPS ####
+    Connect-AzureAD -Credential $psCred
+
+    ## SharePoint KeyVault Azure AD App
+    $KvAppIdentifier = "https://$tenantName/91ec1750-8357-47e9-a04c-50a4b32fd5db"
+    $KvAppName = "Onboarding Checker Keyvault"
+    # delegated permissions for Windows Azure AD
+    $req1 = New-Object -TypeName "Microsoft.Open.AzureAD.Model.RequiredResourceAccess"
+    $req1.ResourceAppId = "00000002-0000-0000-c000-000000000000"
+    $acc1r1 = New-Object -TypeName "Microsoft.Open.AzureAD.Model.ResourceAccess" -ArgumentList "311a71cc-e848-46a1-bdf8-97ff7156d8e6","Scope"
+    $req1.ResourceAccess = $acc1r1
+    $keyvaultApp = EnsureAzureAdApp -AppIdentifier $KvAppIdentifier -AppName $KvAppName -ReplyURLs @("https://localhost") -RequiredPermissions $req1
+    
 } else {
     Write-Host "*************************************"
     Write-Host " Skipping creation of resource group "
